2ae630b844a66df1a7b3abd78a2920c3f9ad9eb8
Jakub Jankiewicz <jcubic@onet.pl>
2025-09-30 22:23:32 +0200

    some base config

--- emacs/base.el
+++ emacs/base.el
@@ -8,6 +8,27 @@
 
 (global-auto-revert-mode t)
 
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
 (setenv "NODE_NO_READLINE" "1")
 (setenv "BROWSER" "brave-browser")
+(setq inferior-js-program-command "node --interactive")
 
+;; fix bug with history and killed buffers
+(add-hook 'kill-buffer-hook
+          (lambda ()
+            (interactive)
+            (setq buffer-name-history (delete (buffer-name)
+                                              buffer-name-history))))
+
+(global-set-key (kbd "C-c C-p") 'fill-paragraph)
+
+(global-set-key (kbd "<C-mouse-1>") (lambda (e)
+                                      (interactive "e")
+                                      (mouse-set-point e)))
+
+(global-set-key (kbd "<C-down-mouse-1>")
+                (lambda (e)
+                  (interactive "e")
+                  (message "C+<mouse>")))
