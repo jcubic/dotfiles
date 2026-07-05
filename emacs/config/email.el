8914c10464b00c5833fd97d4b009ac017e3d2136
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-07-05 21:22:20 +0200

    emacs: fix email config

--- emacs/config/email.el
+++ emacs/config/email.el
@@ -2,16 +2,19 @@
 
 ;; send email: C-x m => C-c C-c
 (if (and (boundp 'SMTP_EMAIL) (boundp 'SMTP_SERVER) (boundp 'SMTP_NAME))
-
-    (setq user-full-name SMTP_NAME
+    (progn
+      (setq user-full-name SMTP_NAME
           user-mail-address SMTP_EMAIL)
 
-  (setq send-mail-function 'smtpmail-send-it
-        message-send-mail-function 'smtpmail-send-it)
+      (setq send-mail-function 'smtpmail-send-it
+            message-send-mail-function 'smtpmail-send-it)
 
-  (setq smtpmail-smtp-server SMTP_SERVER
-        smtpmail-smtp-service 587)
+      (setq smtpmail-smtp-server SMTP_SERVER
+            smtpmail-smtp-service 587)
 
-  (setq smtpmail-stream-type 'starttls)
+      (setq smtpmail-stream-type 'starttls)
 
-  (setq auth-sources '("~/.authinfo.gpg")))
+      (setq auth-sources '("~/.authinfo.gpg"))
+
+      (setq message-default-mail-headers
+            (concat "Bcc: " SMTP_EMAIL "\n" message-default-mail-headers))))
